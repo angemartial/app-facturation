21ef6802a62a891439eaebf415a4c3a77412a876
Ange Martial <angemartialkoffi@gmail.com>
2018-07-16 18:54:38 +0000

    ajout de script js et creation de la page index

--- web/js/script.js
+++ web/js/script.js
@@ -1,3 +1,28 @@
 /**
  * Created by Eliket-Grp on 16/07/2018.
  */
+window.onload = function () {
+    var admin = {
+        init: function () {
+            _.forEach(admin.events, function (callback, eventSpec) {
+                var parts = _.split(eventSpec, ' ');
+                $(document).on(parts[0], _.join(_.slice(parts, 1), ' '), callback);
+            });
+        },
+        events: {
+            'click #left-menu > li a': function (e) {
+                var parent = $(e.currentTarget).parent();
+                var child = parent.find('ul');
+                console.log(parent);
+                if(child.length){
+                    e.preventDefault();
+                    child.slideToggle();
+                    $('#left-menu').find('a').removeClass('drop-open');
+                    $(e.currentTarget).addClass('drop-open');
+                    parent.siblings().find('ul').slideUp();
+                }
+            }
+        }
+    };
+    admin.init();
+};
